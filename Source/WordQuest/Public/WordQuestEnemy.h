19dd482c2228bc355cae62e8dda5b87d5b83b94a
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 20:51:58 +0800

    Add Stage 1 enemy actor

---
+++ Source/WordQuest/Public/WordQuestEnemy.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WordQuestEnemy.generated.h"
+
+class UBoxComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class WORDQUEST_API AWordQuestEnemy : public AActor
+{
+    GENERATED_BODY()
+public:
+    AWordQuestEnemy();
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Word Quest") bool bBoss = false;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Word Quest") int32 MaxHP = 1;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Word Quest") int32 CurrentHP = 1;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Word Quest") int32 WaveNumber = 1;
+
+    UFUNCTION(BlueprintCallable) void ConfigureEnemy(int32 InWave, bool bInBoss);
+    UFUNCTION(BlueprintCallable) bool ReceiveWordDamage(int32 DamageAmount);
+
+    UPROPERTY(VisibleAnywhere) TObjectPtr<UBoxComponent> Trigger;
+    UPROPERTY(VisibleAnywhere) TObjectPtr<UStaticMeshComponent> Visual;
+};
