001fe7ffe47b7072372b828c4ff68bff9e1aa676
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-17 02:03:02 +0800

    Add segmented walking motion to hero widget

--- Source/WordQuest/Public/WordQuestHeroWidget.h
+++ Source/WordQuest/Public/WordQuestHeroWidget.h
@@ -4,6 +4,7 @@
 #include "Blueprint/UserWidget.h"
 #include "WordQuestHeroWidget.generated.h"
 
+class UCanvasPanel;
 class UImage;
 class UTexture2D;
 
@@ -14,19 +15,36 @@ class WORDQUEST_API UWordQuestHeroWidget : public UUserWidget
 
 public:
     virtual void NativeConstruct() override;
+    virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
     void SetFacingLeft(bool bInFacingLeft);
+    void SetMovementState(bool bInMoving, bool bInAir);
 
 protected:
     virtual TSharedRef<SWidget> RebuildWidget() override;
 
 private:
-    void RefreshBrush();
+    void RefreshBrushes();
+    void ApplyBandBrush(UImage* Image, const FBox2f& UVRegion, const FVector2D& ImageSize);
+    void ResetPartTransforms();
 
     UPROPERTY()
-    TObjectPtr<UImage> HeroImage;
+    TObjectPtr<UCanvasPanel> RootPanel;
+
+    UPROPERTY()
+    TObjectPtr<UImage> HeadImage;
+
+    UPROPERTY()
+    TObjectPtr<UImage> TorsoImage;
+
+    UPROPERTY()
+    TObjectPtr<UImage> LegsImage;
 
     UPROPERTY()
     TObjectPtr<UTexture2D> HeroTexture;
 
     bool bFacingLeft = false;
+    bool bMoving = false;
+    bool bAirborne = false;
+    float WalkPhase = 0.f;
 };
