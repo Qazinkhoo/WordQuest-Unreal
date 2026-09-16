fd80943167954f1b4ddd1a6215a13ab41795a40f
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 23:44:54 +0800

    Add one-second answer feedback state

--- Source/WordQuest/Public/WordQuestGameMode.h
+++ Source/WordQuest/Public/WordQuestGameMode.h
@@ -23,6 +23,10 @@ public:
     UPROPERTY(BlueprintReadOnly, Category="Word Quest") bool bShopOpen = false;
     UPROPERTY(BlueprintReadOnly, Category="Word Quest") bool bMainMenuOpen = false;
     UPROPERTY(BlueprintReadOnly, Category="Word Quest") bool bGameOver = false;
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") bool bAnswerFeedbackActive = false;
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") int32 SelectedAnswerIndex = INDEX_NONE;
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") bool bSelectedAnswerCorrect = false;
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") float AnswerFeedbackStartTime = 0.f;
     UPROPERTY(BlueprintReadOnly, Category="Word Quest") FWordQuestQuestion CurrentQuestion;
     UPROPERTY(BlueprintReadOnly, Category="Word Quest") TObjectPtr<AWordQuestEnemy> CurrentEnemy;
 
@@ -47,6 +51,7 @@ public:
 
 private:
     bool LoadDifferentQuestion();
+    void ResolveSelectedAnswer();
     void PlayTone(float StartFrequency, float EndFrequency, float DurationSeconds, float Volume = 0.35f);
     FName GetBaseMapForStage(int32 Stage) const;
 
