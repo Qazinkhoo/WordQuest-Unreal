4126088c757ce9a178d9240adfef714167468e6b
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-17 05:22:29 +0800

    Load Ghast Enderman and Ender Dragon boss sprites

--- Source/WordQuest/Private/WordQuestEnemyWidget.cpp
+++ Source/WordQuest/Private/WordQuestEnemyWidget.cpp
@@ -24,7 +24,14 @@ void UWordQuestEnemyWidget::NativeConstruct()
 
 void UWordQuestEnemyWidget::SetMonsterWave(int32 InWaveNumber)
 {
-    WaveNumber = FMath::Clamp(InWaveNumber, 1, 4);
+    SetEnemyVisual(InWaveNumber, StageNumber, false);
+}
+
+void UWordQuestEnemyWidget::SetEnemyVisual(int32 InWaveNumber, int32 InStageNumber, bool bInBoss)
+{
+    WaveNumber = FMath::Clamp(InWaveNumber, 1, 5);
+    StageNumber = FMath::Clamp(InStageNumber, 1, 3);
+    bBoss = bInBoss;
     RefreshMonster();
 }
 
@@ -34,21 +41,40 @@ void UWordQuestEnemyWidget::RefreshMonster()
 
     const TCHAR* TexturePath = nullptr;
 
-    switch (WaveNumber)
+    if (bBoss)
     {
-    case 1:
-        TexturePath = TEXT("/Game/UI/Enemies/Monster01_Zombie.Monster01_Zombie");
-        break;
-    case 2:
-        TexturePath = TEXT("/Game/UI/Enemies/Monster02_Skeleton.Monster02_Skeleton");
-        break;
-    case 3:
-        TexturePath = TEXT("/Game/UI/Enemies/Monster03_Spider.Monster03_Spider");
-        break;
-    case 4:
-    default:
-        TexturePath = TEXT("/Game/UI/Enemies/Monster04_Creeper.Monster04_Creeper");
-        break;
+        switch (StageNumber)
+        {
+        case 1:
+            TexturePath = TEXT("/Game/UI/Bosses/Boss01_Ghast.Boss01_Ghast");
+            break;
+        case 2:
+            TexturePath = TEXT("/Game/UI/Bosses/Boss02_Enderman.Boss02_Enderman");
+            break;
+        case 3:
+        default:
+            TexturePath = TEXT("/Game/UI/Bosses/Boss03_EnderDragon.Boss03_EnderDragon");
+            break;
+        }
+    }
+    else
+    {
+        switch (FMath::Clamp(WaveNumber, 1, 4))
+        {
+        case 1:
+            TexturePath = TEXT("/Game/UI/Enemies/Monster01_Zombie.Monster01_Zombie");
+            break;
+        case 2:
+            TexturePath = TEXT("/Game/UI/Enemies/Monster02_Skeleton.Monster02_Skeleton");
+            break;
+        case 3:
+            TexturePath = TEXT("/Game/UI/Enemies/Monster03_Spider.Monster03_Spider");
+            break;
+        case 4:
+        default:
+            TexturePath = TEXT("/Game/UI/Enemies/Monster04_Creeper.Monster04_Creeper");
+            break;
+        }
     }
 
     MonsterTexture = LoadObject<UTexture2D>(nullptr, TexturePath);
