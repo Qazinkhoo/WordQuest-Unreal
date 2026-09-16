2b97ad546ae41720c99d06bcc6523089a3918cab
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-17 05:22:18 +0800

    Add stage-specific boss sprite support

--- Source/WordQuest/Public/WordQuestEnemyWidget.h
+++ Source/WordQuest/Public/WordQuestEnemyWidget.h
@@ -14,7 +14,12 @@ class WORDQUEST_API UWordQuestEnemyWidget : public UUserWidget
 
 public:
     virtual void NativeConstruct() override;
+
+    // Retained for compatibility with the existing normal-monster setup.
     void SetMonsterWave(int32 InWaveNumber);
+
+    // Chooses either the normal wave monster or the boss for the current stage.
+    void SetEnemyVisual(int32 InWaveNumber, int32 InStageNumber, bool bInBoss);
 
 protected:
     virtual TSharedRef<SWidget> RebuildWidget() override;
@@ -29,4 +34,6 @@ private:
     TObjectPtr<UTexture2D> MonsterTexture;
 
     int32 WaveNumber = 1;
+    int32 StageNumber = 1;
+    bool bBoss = false;
 };
