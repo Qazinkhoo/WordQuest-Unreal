8698414a24e44083548d981549f446fbbcd07772
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 20:53:17 +0800

    Add Stage 1 sword pickup

---
+++ Source/WordQuest/Public/WordQuestSwordPickup.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WordQuestSwordPickup.generated.h"
+
+class UBoxComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class WORDQUEST_API AWordQuestSwordPickup : public AActor
+{
+    GENERATED_BODY()
+public:
+    AWordQuestSwordPickup();
+protected:
+    virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+public:
+    UPROPERTY(VisibleAnywhere) TObjectPtr<UBoxComponent> Trigger;
+    UPROPERTY(VisibleAnywhere) TObjectPtr<UStaticMeshComponent> Visual;
+};
