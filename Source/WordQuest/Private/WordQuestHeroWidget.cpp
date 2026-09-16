ff6c675465e874b6c40d9c35f70c47322436fc26
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-17 02:49:12 +0800

    Align six-part hero to assembled preview

--- Source/WordQuest/Private/WordQuestHeroWidget.cpp
+++ Source/WordQuest/Private/WordQuestHeroWidget.cpp
@@ -21,7 +21,8 @@ TSharedRef<SWidget> UWordQuestHeroWidget::RebuildWidget()
         return SNullWidget::NullWidget;
     }
 
-    // Back limbs first, then torso/front limbs/head for cleaner layering.
+    // The layout below is taken directly from the assembled reference preview.
+    // Back limbs are drawn first, torso in the middle, then front limbs and head.
     UCanvasPanelSlot* LeftLegSlot = RootPanel->AddChildToCanvas(LeftLegImage);
     UCanvasPanelSlot* LeftArmSlot = RootPanel->AddChildToCanvas(LeftArmImage);
     UCanvasPanelSlot* TorsoSlot = RootPanel->AddChildToCanvas(TorsoImage);
@@ -31,47 +32,48 @@ TSharedRef<SWidget> UWordQuestHeroWidget::RebuildWidget()
 
     if (HeadSlot)
     {
-        HeadSlot->SetPosition(FVector2D(24.f, 0.f));
-        HeadSlot->SetSize(FVector2D(82.f, 69.f));
+        HeadSlot->SetPosition(FVector2D(31.f, 3.f));
+        HeadSlot->SetSize(FVector2D(76.f, 64.f));
         HeadSlot->SetZOrder(6);
     }
     if (TorsoSlot)
     {
-        TorsoSlot->SetPosition(FVector2D(30.f, 61.f));
-        TorsoSlot->SetSize(FVector2D(72.f, 74.f));
+        TorsoSlot->SetPosition(FVector2D(25.f, 62.f));
+        TorsoSlot->SetSize(FVector2D(70.f, 72.f));
         TorsoSlot->SetZOrder(3);
     }
     if (LeftArmSlot)
     {
-        LeftArmSlot->SetPosition(FVector2D(25.f, 73.f));
-        LeftArmSlot->SetSize(FVector2D(29.f, 63.f));
+        LeftArmSlot->SetPosition(FVector2D(18.f, 75.f));
+        LeftArmSlot->SetSize(FVector2D(33.f, 72.f));
         LeftArmSlot->SetZOrder(2);
     }
     if (RightArmSlot)
     {
-        RightArmSlot->SetPosition(FVector2D(76.f, 75.f));
-        RightArmSlot->SetSize(FVector2D(40.f, 64.f));
+        RightArmSlot->SetPosition(FVector2D(58.f, 78.f));
+        RightArmSlot->SetSize(FVector2D(45.f, 72.f));
         RightArmSlot->SetZOrder(5);
     }
     if (LeftLegSlot)
     {
-        LeftLegSlot->SetPosition(FVector2D(42.f, 126.f));
-        LeftLegSlot->SetSize(FVector2D(42.f, 65.f));
+        LeftLegSlot->SetPosition(FVector2D(25.f, 132.f));
+        LeftLegSlot->SetSize(FVector2D(51.f, 78.f));
         LeftLegSlot->SetZOrder(1);
     }
     if (RightLegSlot)
     {
-        RightLegSlot->SetPosition(FVector2D(57.f, 126.f));
-        RightLegSlot->SetSize(FVector2D(46.f, 67.f));
+        RightLegSlot->SetPosition(FVector2D(48.f, 132.f));
+        RightLegSlot->SetSize(FVector2D(54.f, 78.f));
         RightLegSlot->SetZOrder(4);
     }
 
-    HeadImage->SetRenderTransformPivot(FVector2D(0.5f, 0.9f));
+    // Rotate limbs from the shoulder/hip so they remain attached while moving.
+    HeadImage->SetRenderTransformPivot(FVector2D(0.5f, 0.92f));
     TorsoImage->SetRenderTransformPivot(FVector2D(0.5f, 0.5f));
-    LeftArmImage->SetRenderTransformPivot(FVector2D(0.5f, 0.08f));
-    RightArmImage->SetRenderTransformPivot(FVector2D(0.5f, 0.08f));
-    LeftLegImage->SetRenderTransformPivot(FVector2D(0.5f, 0.06f));
-    RightLegImage->SetRenderTransformPivot(FVector2D(0.5f, 0.06f));
+    LeftArmImage->SetRenderTransformPivot(FVector2D(0.58f, 0.08f));
+    RightArmImage->SetRenderTransformPivot(FVector2D(0.42f, 0.08f));
+    LeftLegImage->SetRenderTransformPivot(FVector2D(0.58f, 0.04f));
+    RightLegImage->SetRenderTransformPivot(FVector2D(0.42f, 0.04f));
 
     RootPanel->SetRenderTransformPivot(FVector2D(0.5f, 0.5f));
     return RootPanel->TakeWidget();
@@ -104,10 +106,10 @@ void UWordQuestHeroWidget::NativeTick(const FGeometry& MyGeometry, float InDelta
 
     if (!bMoving && !bAirborne)
     {
-        WalkPhase += InDeltaTime * 2.f;
+        WalkPhase += InDeltaTime * 1.8f;
         ResetPartTransforms();
 
-        const float IdleBob = FMath::Sin(WalkPhase) * 0.6f;
+        const float IdleBob = FMath::Sin(WalkPhase) * 0.35f;
         FWidgetTransform HeadTransform;
         HeadTransform.Scale = FVector2D(1.f, 1.f);
         HeadTransform.Translation = FVector2D(0.f, IdleBob);
@@ -117,77 +119,78 @@ void UWordQuestHeroWidget::NativeTick(const FGeometry& MyGeometry, float InDelta
 
     if (bAirborne)
     {
+        ResetPartTransforms();
+
         FWidgetTransform HeadTransform;
         HeadTransform.Scale = FVector2D(1.f, 1.f);
-        HeadTransform.Translation = FVector2D(0.f, -2.f);
+        HeadTransform.Translation = FVector2D(0.f, -1.5f);
         HeadImage->SetRenderTransform(HeadTransform);
 
         FWidgetTransform TorsoTransform;
         TorsoTransform.Scale = FVector2D(1.f, 1.f);
-        TorsoTransform.Translation = FVector2D(0.f, -2.f);
-        TorsoTransform.Angle = -1.5f;
+        TorsoTransform.Translation = FVector2D(0.f, -1.5f);
         TorsoImage->SetRenderTransform(TorsoTransform);
 
         FWidgetTransform LeftArmTransform;
         LeftArmTransform.Scale = FVector2D(1.f, 1.f);
-        LeftArmTransform.Angle = 25.f;
+        LeftArmTransform.Angle = 11.f;
         LeftArmImage->SetRenderTransform(LeftArmTransform);
 
         FWidgetTransform RightArmTransform;
         RightArmTransform.Scale = FVector2D(1.f, 1.f);
-        RightArmTransform.Angle = -28.f;
+        RightArmTransform.Angle = -12.f;
         RightArmImage->SetRenderTransform(RightArmTransform);
 
         FWidgetTransform LeftLegTransform;
         LeftLegTransform.Scale = FVector2D(1.f, 1.f);
-        LeftLegTransform.Angle = -16.f;
-        LeftLegTransform.Translation = FVector2D(0.f, -3.f);
+        LeftLegTransform.Angle = -7.f;
+        LeftLegTransform.Translation = FVector2D(0.f, -2.f);
         LeftLegImage->SetRenderTransform(LeftLegTransform);
 
         FWidgetTransform RightLegTransform;
         RightLegTransform.Scale = FVector2D(1.f, 1.f);
-        RightLegTransform.Angle = 20.f;
-        RightLegTransform.Translation = FVector2D(0.f, -5.f);
+        RightLegTransform.Angle = 8.f;
+        RightLegTransform.Translation = FVector2D(0.f, -3.f);
         RightLegImage->SetRenderTransform(RightLegTransform);
         return;
     }
 
-    WalkPhase += InDeltaTime * 9.5f;
+    WalkPhase += InDeltaTime * 8.f;
     const float S = FMath::Sin(WalkPhase);
     const float Lift = FMath::Abs(FMath::Sin(WalkPhase * 2.f));
 
     FWidgetTransform HeadTransform;
     HeadTransform.Scale = FVector2D(1.f, 1.f);
-    HeadTransform.Translation = FVector2D(S * 0.4f, -Lift * 0.8f);
-    HeadTransform.Angle = S * 0.7f;
+    HeadTransform.Translation = FVector2D(0.f, -Lift * 0.45f);
+    HeadTransform.Angle = S * 0.35f;
     HeadImage->SetRenderTransform(HeadTransform);
 
     FWidgetTransform TorsoTransform;
     TorsoTransform.Scale = FVector2D(1.f, 1.f);
-    TorsoTransform.Translation = FVector2D(S * 0.7f, -Lift * 1.2f);
-    TorsoTransform.Angle = S * 1.5f;
+    TorsoTransform.Translation = FVector2D(0.f, -Lift * 0.75f);
+    TorsoTransform.Angle = S * 0.55f;
     TorsoImage->SetRenderTransform(TorsoTransform);
 
     FWidgetTransform LeftArmTransform;
     LeftArmTransform.Scale = FVector2D(1.f, 1.f);
-    LeftArmTransform.Angle = S * 19.f;
+    LeftArmTransform.Angle = S * 11.f;
     LeftArmImage->SetRenderTransform(LeftArmTransform);
 
     FWidgetTransform RightArmTransform;
     RightArmTransform.Scale = FVector2D(1.f, 1.f);
-    RightArmTransform.Angle = -S * 19.f;
+    RightArmTransform.Angle = -S * 11.f;
     RightArmImage->SetRenderTransform(RightArmTransform);
 
     FWidgetTransform LeftLegTransform;
     LeftLegTransform.Scale = FVector2D(1.f, 1.f);
-    LeftLegTransform.Angle = -S * 18.f;
-    LeftLegTransform.Translation = FVector2D(0.f, -FMath::Max(0.f, S) * 2.5f);
+    LeftLegTransform.Angle = -S * 7.f;
+    LeftLegTransform.Translation = FVector2D(0.f, -FMath::Max(0.f, S) * 1.5f);
     LeftLegImage->SetRenderTransform(LeftLegTransform);
 
     FWidgetTransform RightLegTransform;
     RightLegTransform.Scale = FVector2D(1.f, 1.f);
-    RightLegTransform.Angle = S * 18.f;
-    RightLegTransform.Translation = FVector2D(0.f, -FMath::Max(0.f, -S) * 2.5f);
+    RightLegTransform.Angle = S * 7.f;
+    RightLegTransform.Translation = FVector2D(0.f, -FMath::Max(0.f, -S) * 1.5f);
     RightLegImage->SetRenderTransform(RightLegTransform);
 }
 
@@ -227,27 +230,27 @@ void UWordQuestHeroWidget::RefreshBrushes()
 
     ApplyPartBrush(HeadImage,
         FBox2f(FVector2f(0.015625f, 0.020833f), FVector2f(0.300781f, 0.341146f)),
-        FVector2D(146.f, 123.f));
+        FVector2D(76.f, 64.f));
 
     ApplyPartBrush(TorsoImage,
         FBox2f(FVector2f(0.332031f, 0.020833f), FVector2f(0.583984f, 0.364583f)),
-        FVector2D(129.f, 132.f));
+        FVector2D(70.f, 72.f));
 
     ApplyPartBrush(LeftArmImage,
         FBox2f(FVector2f(0.625000f, 0.020833f), FVector2f(0.757813f, 0.403646f)),
-        FVector2D(68.f, 147.f));
+        FVector2D(33.f, 72.f));
 
     ApplyPartBrush(RightArmImage,
         FBox2f(FVector2f(0.789063f, 0.020833f), FVector2f(0.968750f, 0.403646f)),
-        FVector2D(92.f, 147.f));
+        FVector2D(45.f, 72.f));
 
     ApplyPartBrush(LeftLegImage,
         FBox2f(FVector2f(0.015625f, 0.468750f), FVector2f(0.177734f, 0.799479f)),
-        FVector2D(83.f, 127.f));
+        FVector2D(51.f, 78.f));
 
     ApplyPartBrush(RightLegImage,
         FBox2f(FVector2f(0.214844f, 0.468750f), FVector2f(0.394531f, 0.809896f)),
-        FVector2D(92.f, 131.f));
+        FVector2D(54.f, 78.f));
 }
 
 void UWordQuestHeroWidget::ResetPartTransforms()
