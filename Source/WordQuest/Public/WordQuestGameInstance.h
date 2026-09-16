2e54db1ccf31763d27db2712f9cf5b1bc70cdffb
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 17:55:45 +0800

    Add persistent Word Quest game state interface

---
+++ Source/WordQuest/Public/WordQuestGameInstance.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "WordQuestTypes.h"
+#include "WordQuestGameInstance.generated.h"
+
+UCLASS()
+class WORDQUEST_API UWordQuestGameInstance : public UGameInstance
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") FWordQuestPlayerState PlayerState;
+
+    UFUNCTION(BlueprintCallable, Category="Word Quest") void StartNewAdventure();
+    UFUNCTION(BlueprintCallable, Category="Word Quest") void CollectSword();
+    UFUNCTION(BlueprintCallable, Category="Word Quest") bool ApplyEnemyHit();
+    UFUNCTION(BlueprintCallable, Category="Word Quest") void RewardNormalEnemy();
+    UFUNCTION(BlueprintCallable, Category="Word Quest") void RewardBoss();
+    UFUNCTION(BlueprintCallable, Category="Word Quest|Shop") bool BuyApple();
+    UFUNCTION(BlueprintCallable, Category="Word Quest|Shop") bool BuyStar();
+    UFUNCTION(BlueprintCallable, Category="Word Quest|Shop") bool BuyArmour();
+    UFUNCTION(BlueprintPure, Category="Word Quest") bool IsGameOver() const;
+
+private:
+    bool bAppleBoughtThisVisit = false;
+    bool bStarBoughtThisVisit = false;
+    bool bArmourBoughtThisVisit = false;
+
+public:
+    UFUNCTION(BlueprintCallable, Category="Word Quest|Shop") void ResetShopStock();
+};
