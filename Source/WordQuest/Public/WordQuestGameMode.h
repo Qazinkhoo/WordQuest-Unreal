10ecf4a0095a8b45fbcc4de015e3548aa2e585e5
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 20:52:38 +0800

    Add Stage 1 game mode

---
+++ Source/WordQuest/Public/WordQuestGameMode.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WordQuestTypes.h"
+#include "WordQuestGameMode.generated.h"
+
+class AWordQuestEnemy;
+
+UCLASS()
+class WORDQUEST_API AWordQuestGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+public:
+    AWordQuestGameMode();
+    virtual void BeginPlay() override;
+
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") int32 CurrentWave = 1;
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") bool bBattleActive = false;
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") bool bStageClear = false;
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") FWordQuestQuestion CurrentQuestion;
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") TObjectPtr<AWordQuestEnemy> CurrentEnemy;
+
+    UFUNCTION(BlueprintCallable) void StartEncounter(AWordQuestEnemy* Enemy);
+    UFUNCTION(BlueprintCallable) bool SubmitAnswer(int32 AnswerIndex);
+    UFUNCTION(BlueprintCallable) void AdvanceWave();
+
+    UFUNCTION(BlueprintImplementableEvent) void OnQuestionChanged();
+    UFUNCTION(BlueprintImplementableEvent) void OnBattleStateChanged();
+    UFUNCTION(BlueprintImplementableEvent) void OnStageCleared();
+    UFUNCTION(BlueprintImplementableEvent) void OnGameOver();
+
+private:
+    bool LoadDifferentQuestion();
+};
