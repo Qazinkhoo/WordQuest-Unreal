e8f8e03096410403a1d3a42586d1be560d9a80ab
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 22:23:01 +0800

    Route number keys to battle or shop

--- Source/WordQuest/Private/WordQuestPlayerController.cpp
+++ Source/WordQuest/Private/WordQuestPlayerController.cpp
@@ -9,16 +9,57 @@ void AWordQuestPlayerController::SetupInputComponent()
 
     if (!InputComponent) return;
 
-    InputComponent->BindKey(EKeys::One, IE_Pressed, this, &AWordQuestPlayerController::SubmitAnswer1);
-    InputComponent->BindKey(EKeys::Two, IE_Pressed, this, &AWordQuestPlayerController::SubmitAnswer2);
-    InputComponent->BindKey(EKeys::Three, IE_Pressed, this, &AWordQuestPlayerController::SubmitAnswer3);
-    InputComponent->BindKey(EKeys::Four, IE_Pressed, this, &AWordQuestPlayerController::SubmitAnswer4);
+    InputComponent->BindKey(EKeys::One, IE_Pressed, this, &AWordQuestPlayerController::PressOption1);
+    InputComponent->BindKey(EKeys::Two, IE_Pressed, this, &AWordQuestPlayerController::PressOption2);
+    InputComponent->BindKey(EKeys::Three, IE_Pressed, this, &AWordQuestPlayerController::PressOption3);
+    InputComponent->BindKey(EKeys::Four, IE_Pressed, this, &AWordQuestPlayerController::PressOption4);
+    InputComponent->BindKey(EKeys::Enter, IE_Pressed, this, &AWordQuestPlayerController::PressEnter);
 }
 
-void AWordQuestPlayerController::SubmitAnswer1() { SubmitAnswerIndex(0); }
-void AWordQuestPlayerController::SubmitAnswer2() { SubmitAnswerIndex(1); }
-void AWordQuestPlayerController::SubmitAnswer3() { SubmitAnswerIndex(2); }
-void AWordQuestPlayerController::SubmitAnswer4() { SubmitAnswerIndex(3); }
+void AWordQuestPlayerController::PressOption1()
+{
+    if (!GetWorld()) return;
+    if (AWordQuestGameMode* GM = GetWorld()->GetAuthGameMode<AWordQuestGameMode>())
+    {
+        if (GM->bShopOpen) GM->BuyShopApple(); else SubmitAnswerIndex(0);
+    }
+}
+
+void AWordQuestPlayerController::PressOption2()
+{
+    if (!GetWorld()) return;
+    if (AWordQuestGameMode* GM = GetWorld()->GetAuthGameMode<AWordQuestGameMode>())
+    {
+        if (GM->bShopOpen) GM->BuyShopStar(); else SubmitAnswerIndex(1);
+    }
+}
+
+void AWordQuestPlayerController::PressOption3()
+{
+    if (!GetWorld()) return;
+    if (AWordQuestGameMode* GM = GetWorld()->GetAuthGameMode<AWordQuestGameMode>())
+    {
+        if (GM->bShopOpen) GM->BuyShopArmour(); else SubmitAnswerIndex(2);
+    }
+}
+
+void AWordQuestPlayerController::PressOption4()
+{
+    if (!GetWorld()) return;
+    if (AWordQuestGameMode* GM = GetWorld()->GetAuthGameMode<AWordQuestGameMode>())
+    {
+        if (GM->bShopOpen) GM->LeaveStageShop(); else SubmitAnswerIndex(3);
+    }
+}
+
+void AWordQuestPlayerController::PressEnter()
+{
+    if (!GetWorld()) return;
+    if (AWordQuestGameMode* GM = GetWorld()->GetAuthGameMode<AWordQuestGameMode>())
+    {
+        if (GM->bShopOpen) GM->LeaveStageShop();
+    }
+}
 
 void AWordQuestPlayerController::SubmitAnswerIndex(int32 AnswerIndex)
 {
