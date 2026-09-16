70ba092a723cce6b150ee990a32e0ca5cc4a6a84
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 22:28:36 +0800

    Add reliable screen-space combat feedback

--- Source/WordQuest/Public/WordQuestHUD.h
+++ Source/WordQuest/Public/WordQuestHUD.h
@@ -4,6 +4,15 @@
 #include "GameFramework/HUD.h"
 #include "WordQuestHUD.generated.h"
 
+struct FWordQuestHUDMessage
+{
+    FString Text;
+    FColor Color = FColor::White;
+    FVector WorldLocation = FVector::ZeroVector;
+    float StartTime = 0.f;
+    float Duration = 1.35f;
+};
+
 UCLASS()
 class WORDQUEST_API AWordQuestHUD : public AHUD
 {
@@ -11,4 +20,10 @@ class WORDQUEST_API AWordQuestHUD : public AHUD
 
 public:
     virtual void DrawHUD() override;
+
+    void AddFloatingMessage(const FString& InText, const FColor& InColor, const FVector& InWorldLocation, float InDuration = 1.35f);
+
+private:
+    TArray<FWordQuestHUDMessage> FloatingMessages;
+    void DrawFloatingMessages();
 };
