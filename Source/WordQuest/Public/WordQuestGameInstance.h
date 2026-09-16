67fe75bb479abf5fef3f05ba9663971f10eaf305
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 22:21:40 +0800

    Expose shop availability state

--- Source/WordQuest/Public/WordQuestGameInstance.h
+++ Source/WordQuest/Public/WordQuestGameInstance.h
@@ -23,6 +23,13 @@ public:
     UFUNCTION(BlueprintCallable, Category="Word Quest|Shop") bool BuyArmour();
     UFUNCTION(BlueprintPure, Category="Word Quest") bool IsGameOver() const;
 
+    UFUNCTION(BlueprintPure, Category="Word Quest|Shop") bool IsAppleBoughtThisVisit() const { return bAppleBoughtThisVisit; }
+    UFUNCTION(BlueprintPure, Category="Word Quest|Shop") bool IsStarBoughtThisVisit() const { return bStarBoughtThisVisit; }
+    UFUNCTION(BlueprintPure, Category="Word Quest|Shop") bool IsArmourBoughtThisVisit() const { return bArmourBoughtThisVisit; }
+    UFUNCTION(BlueprintPure, Category="Word Quest|Shop") bool CanBuyApple() const;
+    UFUNCTION(BlueprintPure, Category="Word Quest|Shop") bool CanBuyStar() const;
+    UFUNCTION(BlueprintPure, Category="Word Quest|Shop") bool CanBuyArmour() const;
+
 private:
     bool bAppleBoughtThisVisit = false;
     bool bStarBoughtThisVisit = false;
