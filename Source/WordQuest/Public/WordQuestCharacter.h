8ec5b51c78709456c8ec627b4fc13da718310d2c
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 21:38:48 +0800

    Add visible voxel player components

--- Source/WordQuest/Public/WordQuestCharacter.h
+++ Source/WordQuest/Public/WordQuestCharacter.h
@@ -6,6 +6,7 @@
 
 class USpringArmComponent;
 class UCameraComponent;
+class UStaticMeshComponent;
 
 UCLASS()
 class WORDQUEST_API AWordQuestCharacter : public ACharacter
@@ -13,13 +14,23 @@ class WORDQUEST_API AWordQuestCharacter : public ACharacter
     GENERATED_BODY()
 public:
     AWordQuestCharacter();
+    virtual void BeginPlay() override;
     virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly) TObjectPtr<USpringArmComponent> CameraBoom;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly) TObjectPtr<UCameraComponent> SideCamera;
 
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Voxel Visual") TObjectPtr<UStaticMeshComponent> BlockBody;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Voxel Visual") TObjectPtr<UStaticMeshComponent> BlockHead;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Voxel Visual") TObjectPtr<UStaticMeshComponent> LeftArm;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Voxel Visual") TObjectPtr<UStaticMeshComponent> RightArm;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Voxel Visual") TObjectPtr<UStaticMeshComponent> LeftLeg;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Voxel Visual") TObjectPtr<UStaticMeshComponent> RightLeg;
+
     UFUNCTION(BlueprintCallable) void CollectSword();
+    UFUNCTION(BlueprintCallable) void SetBattleLocked(bool bLocked);
 
 private:
     void MoveRight(float Value);
+    bool bBattleLocked = false;
 };
