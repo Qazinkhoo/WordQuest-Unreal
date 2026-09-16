34d5c8d5ba22a7cb622561cb03908879edde8d9a
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 22:22:14 +0800

    Add Stage 1 shop state and actions

--- Source/WordQuest/Public/WordQuestGameMode.h
+++ Source/WordQuest/Public/WordQuestGameMode.h
@@ -18,12 +18,18 @@ public:
     UPROPERTY(BlueprintReadOnly, Category="Word Quest") int32 CurrentWave = 1;
     UPROPERTY(BlueprintReadOnly, Category="Word Quest") bool bBattleActive = false;
     UPROPERTY(BlueprintReadOnly, Category="Word Quest") bool bStageClear = false;
+    UPROPERTY(BlueprintReadOnly, Category="Word Quest") bool bShopOpen = false;
     UPROPERTY(BlueprintReadOnly, Category="Word Quest") FWordQuestQuestion CurrentQuestion;
     UPROPERTY(BlueprintReadOnly, Category="Word Quest") TObjectPtr<AWordQuestEnemy> CurrentEnemy;
 
     UFUNCTION(BlueprintCallable) void StartEncounter(AWordQuestEnemy* Enemy);
     UFUNCTION(BlueprintCallable) bool SubmitAnswer(int32 AnswerIndex);
     UFUNCTION(BlueprintCallable) void AdvanceWave();
+    UFUNCTION(BlueprintCallable) void OpenStageShop();
+    UFUNCTION(BlueprintCallable) void BuyShopApple();
+    UFUNCTION(BlueprintCallable) void BuyShopStar();
+    UFUNCTION(BlueprintCallable) void BuyShopArmour();
+    UFUNCTION(BlueprintCallable) void LeaveStageShop();
 
     UFUNCTION(BlueprintImplementableEvent) void OnQuestionChanged();
     UFUNCTION(BlueprintImplementableEvent) void OnBattleStateChanged();
