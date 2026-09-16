d058f5be76aac308ef58ecbcc1b7db7e3cec81ae
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-16 17:55:06 +0800

    Add Word Quest Unreal module build configuration

---
+++ Source/WordQuest/WordQuest.Build.cs
@@ -0,0 +1,10 @@
+using UnrealBuildTool;
+
+public class WordQuest : ModuleRules
+{
+    public WordQuest(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "Slate", "SlateCore", "Paper2D" });
+    }
+}
