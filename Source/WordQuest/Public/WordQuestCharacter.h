7a7ca037b104419af19327c50ea0bab3cf7c80e3
Qazinkhoo <castzinc.cool91@gmail.com>
2026-09-17 01:40:34 +0800

    Attach hero widget to player character

--- Source/WordQuest/Public/WordQuestCharacter.h
+++ Source/WordQuest/Public/WordQuestCharacter.h
@@ -7,6 +7,8 @@
 class USpringArmComponent;
 class UCameraComponent;
 class UStaticMeshComponent;
+class UWidgetComponent;
+class UWordQuestHeroWidget;
 
 UCLASS()
 class WORDQUEST_API AWordQuestCharacter : public ACharacter
@@ -15,10 +17,13 @@ class WORDQUEST_API AWordQuestCharacter : public ACharacter
 public:
     AWordQuestCharacter();
     virtual void BeginPlay() override;
+    virtual void Tick(float DeltaSeconds) override;
     virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly) TObjectPtr<USpringArmComponent> CameraBoom;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly) TObjectPtr<UCameraComponent> SideCamera;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Hero Visual") TObjectPtr<UWidgetComponent> HeroWidgetComponent;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Voxel Visual") TObjectPtr<UStaticMeshComponent> BlockBody;
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Voxel Visual") TObjectPtr<UStaticMeshComponent> BlockHead;
@@ -35,4 +40,8 @@ public:
 private:
     void MoveRight(float Value);
     bool bBattleLocked = false;
+    bool bFacingLeft = false;
+
+    UPROPERTY()
+    TObjectPtr<UWordQuestHeroWidget> HeroWidget;
 };
